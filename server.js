d7eaee102b816fa5cbba62c23aa6d369f7dcaec9
WLRdotcom <wlratkowski@gmail.com>
2024-09-16 20:39:27 -0600

    Fix file path issue

--- server.js
+++ server.js
@@ -215,6 +215,6 @@ app.post('/create-private-room', async (req, res) => {
 });
 
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
