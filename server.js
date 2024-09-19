2048d7d9f6b79edce604cbde501eeb1258903bcf
Slurpguppy <166891242+Slurpguppy@users.noreply.github.com>
2024-09-19 09:15:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -215,4 +215,7 @@ app.post('/create-private-room', async (req, res) => {
   }
 });
 
-
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
